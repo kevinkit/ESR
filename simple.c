1e19cabcd9fb5b87db93d13f21a51659e05ff9a2
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2016-05-30 00:08:01 +0200

    Create simple.c

---
+++ simple.c
@@ -0,0 +1,59 @@
+/*
+ * simple.c
+ *
+ *This is a universal test for reading and writing
+ *This
+ *  Created on: 29.05.2016
+ *      Author: Kevin
+ */
+
+
+#include <stdlib.h>
+#include "xparameters.h"
+
+
+int main()
+{
+
+
+	//eventuell hier ein Skript schreiben, dass einem die
+	//Dateien erstellt
+	int* own_a = NULL;
+	int own_v = 0;
+
+	int* ext_a = NULL;
+	int ext_v = 0;
+
+
+	#if XPAR_CPU_ID == 0
+		//Write to own memory
+		own_a = (int*) XPAR_MICROBLAZE_0_LMB_BRAM_IF_CNTRL_MICROBLAZE_0_BASEADDR;
+		*own_a = 20;
+		//Read from own memory
+		own_v = *own_a;
+	
+		
+		//Write to extern memory
+		ext_a = (int*) XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + 0x000000001;
+		*ext_a = 30;
+		ext_v = *ext_a;
+
+	#elif XPAR_CPU_ID == 1
+		//Write to own memory
+		own_a = (int*) XPAR_MICROBLAZE_1_LMB_BRAM_IF_CNTRL_MICROBLAZE_1_BASEADDR;
+		*own_a = 20;
+		//Read from own memory
+		own_v = *own_a;
+
+		//Write to extern memory
+		ext_a = (int*) XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + 0x000000001;
+		*ext_a = 30;
+		ext_v = *ext_a;
+	#elif XPAR_CPU_ID == 2
+		// to be continued...
+	#elif XPAR_CPU_ID == 3
+		// to be continued...
+	#endif
+
+	return 0;
+}
