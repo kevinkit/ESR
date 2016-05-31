b3e18406bb6ed4a8172b539a75b0dde56899a0ed
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2016-05-31 16:23:55 +0200

    Update hello_new.c
    works now for MB

--- hello_new.c
+++ hello_new.c
@@ -3,32 +3,9 @@
 #include "xil_io.h"
 #include "xparameters.h"
 
-//void print(char *str);
-
-//extern void Xil_Out16LE(u32 Addr, u16 Value);
-//extern void Xil_Out32LE(u32 Addr, u32 Value);
-
-//Uartlite XPAR_AXI_UARTLITE_0_BASEADDR
-void send_to_Uart(u32 Value)
-{
-	Xil_Out32LE(XPAR_AXI_UARTLITE_0_BASEADDR,Value);
-}
-
-void send_address_to_UART(u32 Addr)
-{
-	u32 val = Xil_In32LE(Addr);
-	send_to_Uart(val);
-}
-
-
 int main()
 {
     init_platform();
-
-    //May not work ? (AXI is mapped here but a
-    //value only is given
-    send_to_Uart((u32) 5);
-
 
 	u32 own_a;
 	int own_v;
@@ -40,14 +17,27 @@ int main()
 
 
 	#if XPAR_CPU_ID == 0
+		for(t = 0; t < (XPAR_CPU_ID*5000000); t++)
+		{
+			t;
+		}
+
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+
+
+
 		//Write to own memory
-		own_a = (u32) XPAR_MICROBLAZE_0_LMB_BRAM_IF_CNTRL_MICROBLAZE_0_BASEADDR;
+		own_a = (u32) XPAR_LM_BRAM_IF_CNTLR_0_BASEADDR;
 		Xil_Out32LE(own_a,20);
-		//*own_a = 20;
+
 		//REad From own memory to see if it worked
-		xil_printf("Wrote to adress %d\n",own_a);
+
+
+		xil_printf("Wrote to adress %x\n",own_a);
 		own_v = (int) Xil_In32LE(own_a);
-		xil_printf("Value read:%d",own_v);
+
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\t\n",own_v);
 
 		if(own_v == 20){
 			print("READ/WRITE to own Memory was correct\n");
@@ -58,9 +48,11 @@ int main()
 		//Read from own memory
 		//*own_v = *own_a;
 
+		print("\n------------\n");
 
 		//Write to extern memory
-		ext_a = (u32) (XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + 1);
+		ext_a = (u32) (XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + 4);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
 		xil_printf("value of baseaddr %x \n", XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR);
 		Xil_Out32LE(ext_a,55);
 		for(t = 0; t < 10; t++)
@@ -68,45 +60,137 @@ int main()
 			t;
 		}
 		//ext_v = (int*) XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + 0x000000002;
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
 		xil_printf("Wrote to ext adress %x\n", ext_a);
 		ext_v = (int) Xil_In32LE(ext_a);
-		xil_printf("Value read:%x\n",ext_v);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",ext_v);
 
-		if(ext_v == 30){
-			print("READ/WRITE to ext Memory was correct");
+		if(ext_v == 55){
+			print("READ/WRITE to ext Memory was correct\n");
 		}
-
+		print("");
 
 	#elif XPAR_CPU_ID == 1
-		//Write to own memory
-		own_a = (u32) XPAR_MICROBLAZE_1_LMB_BRAM_IF_CNTRL_MICROBLAZE_1_BASEADDR;
-		Xil_Out32LE(own_a,20);
-		//*own_a = 20;
-		//REad From own memory to see if it worked
-		xil_printf("Wrote to adress %d\n",(u32) own_a);
-		own_v = (int) Xil_In32LE(own_a);
-		xil_printf("Value read:%d",own_v);
-
-		if(own_v == 20){
-			print("\nREAD/WRITE to own Memory was correct");
+		for(t = 0; t < (XPAR_CPU_ID*5000000); t++)
+		{
+			t;
 		}
 
+		{
+			t;
+		}
 
-		//own_v = (int*) XPAR_MICROBLAZE_0_LMB_BRAM_IF_CNTRL_MICROBLAZE_0_BASEADDR + 0x00000001;
-		//Read from own memory
-		//*own_v = *own_a;
+		xil_printf("PROCESSOR: %d:\t",XPAR_CPU_ID);
+
+
+		//Write to own memory
+		own_a = (u32) XPAR_LM_BRAM_IF_CNTLR_1_BASEADDR;
+		Xil_Out32LE(own_a,35);
+
+		//REad From own memory to see if it worked
+		xil_printf("Wrote to adress %x\n",(u32) own_a);
+		own_v = (int) Xil_In32LE(own_a);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",own_v);
+
+		if(own_v == 35){
+			print("\nREAD/WRITE to own Memory was correct\n");
+		}
 
 
 		//Write to extern memory
-		ext_a = (u32) XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + 0x000000001;
-		Xil_Out32LE(ext_a,30);
-		//ext_v = (int*) XPAR_AXI_BRAM_CTRL_1_S_AXI_BASEADDR + 0x000000002;
-		xil_printf("Wrote to ext adress %d\n",(u32) ext_a);
+		ext_a = (u32) XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + 4;
+		Xil_Out32LE(ext_a,53);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Wrote to ext adress %x\n",(u32) ext_a);
 		ext_v = (int) Xil_In32LE(ext_a);
-		xil_printf("Value read:%d",ext_v);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",ext_v);
 
-		if(ext_v == 30){
-			print("READ/WRITE to ext Memory was correct");
+		if(ext_v == 53){
+			print("READ/WRITE to ext Memory was correct\n");
+		}
+
+	#elif XPAR_CPU_ID == 2
+		for(t = 0; t < (XPAR_CPU_ID*5000000); t++)
+		{
+			t;
+		}
+
+		{
+			t;
+		}
+
+		xil_printf("PROCESSOR: %d:\t",XPAR_CPU_ID);
+
+
+		//Write to own memory
+		own_a = (u32) XPAR_LM_BRAM_IF_CNTLR_2_BASEADDR;
+		Xil_Out32LE(own_a,35);
+		//*own_a = 20;
+		//REad From own memory to see if it worked
+		xil_printf("Wrote to adress %x\n",(u32) own_a);
+		own_v = (int) Xil_In32LE(own_a);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",own_v);
+
+		if(own_v == 35){
+			print("\nREAD/WRITE to own Memory was correct\n");
+		}
+
+
+		//Write to extern memory
+		ext_a = (u32) XPAR_AXI_BRAM_CTRL_3_S_AXI_BASEADDR + 4;
+		Xil_Out32LE(ext_a,53);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Wrote to ext adress %x\n",(u32) ext_a);
+		ext_v = (int) Xil_In32LE(ext_a);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",ext_v);
+
+		if(ext_v == 53){
+			print("READ/WRITE to ext Memory was correct\n");
+		}
+	#elif XPAR_CPU_ID == 3
+		for(t = 0; t < (XPAR_CPU_ID*5000000); t++)
+		{
+			t;
+		}
+
+		{
+			t;
+		}
+
+		xil_printf("PROCESSOR: %d:\t",XPAR_CPU_ID);
+
+
+		//Write to own memory
+		own_a = (u32) XPAR_LM_BRAM_IF_CNTLR_3_BASEADDR;
+		Xil_Out32LE(own_a,35);
+
+		//REad From own memory to see if it worked
+		xil_printf("Wrote to adress %x\n",(u32) own_a);
+		own_v = (int) Xil_In32LE(own_a);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",own_v);
+
+		if(own_v == 35){
+			print("\nREAD/WRITE to own Memory was correct\n");
+		}
+
+
+		//Write to extern memory
+		ext_a = (u32) XPAR_AXI_BRAM_CTRL_2_S_AXI_BASEADDR + 4;
+		Xil_Out32LE(ext_a,53);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Wrote to ext adress %x\n",(u32) ext_a);
+		ext_v = (int) Xil_In32LE(ext_a);
+		xil_printf("PROCESSOR: %d",XPAR_CPU_ID);
+		xil_printf("Value read:%d\n",ext_v);
+
+		if(ext_v == 53){
+			print("READ/WRITE to ext Memory was correct\n");
 		}
 	#endif
 
